9ebe7940711f81cab87248ec31ae6a489dc9a2bd
avizaguri-macbook <avi210892@gmail.com>
2025-05-19 22:30:03 +0300

    Bump package version to 0.0.3 in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-package_version = '0.0.2'
+package_version = '0.0.3'
 
 package_name = 'python-notion-plus'
 package_description = 'Python wrapper for Notion API'
